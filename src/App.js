bfdcb5c03cd740cfce61981a2a4ed71a6e30cd66
Abhijeetkulkarni <devloxtech@gmail.com>
2025-06-22 22:50:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Services from './pages/Services';
+import Blogs from './pages/Blogs';
+import Portfolio from './pages/Portfolio';
+import Whywebsite2025 from './pages/Whywebsite2025';
+import AiAndUXBlog from './pages/AiAndUXBlogjs';
+import StartupMistakesBlog from './pages/StartupMistakesBlog'; 
+import GooglePasswordManager from './pages/GooglePasswordManager';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/Whywebsite2025" element={<Whywebsite2025/>} />
+          <Route path="/AiAndUXBlog" element={<AiAndUXBlog/>} />
+          <Route path="/StartupMistakesBlog" element={<StartupMistakesBlog/>}/>
+          <Route path="/GooglePasswordManager" element={<GooglePasswordManager/>}/>
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
